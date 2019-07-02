7b8865c1eed4936e3586b079c0d87d4eb64478de
Faithtofaith76 <47899272+Faithtofaith76@users.noreply.github.com>
2019-07-02 11:58:09 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -0,0 +1,63 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Job;
+import org.launchcode.models.forms.JobForm;
+import org.launchcode.models.data.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping(value = "job")
+public class JobController {
+
+    private JobData jobData = JobData.getInstance();
+
+    // The detail display for a given Job at URLs like /job?id=17
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, int id) {
+
+        // TODO #1 - get the Job with the given ID and pass it into the view
+        Job someJob = jobData.findById(id);
+        model.addAttribute("job", someJob);
+
+        return "job-detail";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new JobForm());
+        return "new-job";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid JobForm jobForm, Errors errors) {
+
+        // TODO #6 - Validate the JobForm model, and if valid, create a
+        // new Job and add it to the jobData data store. Then
+        // redirect to the job detail view for the new Job.
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add JobForm");
+            return "job";
+        }
+        Job newJob = new Job();
+        newJob.setName(jobForm.getName());
+        newJob.setEmployer(jobForm.getEmployers().findById(jobForm.getEmployerId()));
+        newJob.setLocation(jobForm.getLocations().findById(jobForm.getLocations()));
+        newJob.setCoreCompetency(jobForm.getCoreCompetencies().findById(jobForm.getCoreCompetencies()));
+        newJob.setPositionType(jobForm.getPositionTypes().findById(jobForm.getPositionTypes()));
+        JobData.add(newJob);
+             return "new-job";
+
+        return "";
+
+    }
+}
